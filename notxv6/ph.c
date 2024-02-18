adcd7ba9951f16dc92507cba626ca930f5a63fed
Lanssi <962673761@qq.com>
2024-02-18 21:41:03 +0800

    lab thread pass all tests!

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,7 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
-
+pthread_mutex_t lock[NBUCKET];
 
 double
 now()
@@ -52,7 +52,11 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    // Added by lanssi
+    // lock and unlock
+    pthread_mutex_lock(&lock[i]);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock[i]);
   }
 
 }
@@ -117,6 +121,11 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
+
+  // Added by lanssi
+  // init the lock
+  for (int i = 0; i < NBUCKET; i++)
+    pthread_mutex_init(&lock[i], NULL);
 
   //
   // first the puts
